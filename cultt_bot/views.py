d225da7805b0659b3ad886fbe45dbf2e8170a562
ilanikiforov <Stalker74369@yandex.ru>
2022-07-28 04:25:45 +0300

    Выбор бренда

--- cultt_bot/views.py
+++ cultt_bot/views.py
@@ -24,43 +24,40 @@ def web_hook_bot(request, bot_url):
         if request.method == "POST":
             data = json.loads(request.body.decode('utf-8'))
 
-            try:
-                # Если пользователь нажал кнопку
-                if 'callback_query' in data:
-                    chat_id = data['callback_query']['from']['id']
-                    chat_data = data['callback_query']['data']
-                    message_id = data['callback_query']['message']['message_id']
+            # Если пользователь нажал кнопку
+            if 'callback_query' in data:
+                chat_id = data['callback_query']['from']['id']
+                chat_data = data['callback_query']['data']
+                message_id = data['callback_query']['message']['message_id']
 
-                    bot_logic(telegram_bot.id, chat_id, chat_data, 'data', message_id)
+                bot_logic(telegram_bot.id, chat_id, chat_data, 'data', message_id)
 
-                # Если пользователь написал что-то
-                if 'message' in data:
-                    if 'message' in data['message'].keys():
-                        chat_id = data['message']['chat']['id']
-                        chat_msg = data['message']['text']
-                        message_id = data['message']['message_id']
+            # Если пользователь написал что-то
+            if 'message' in data:
+                if 'message' in data['message'].keys():
+                    chat_id = data['message']['chat']['id']
+                    chat_msg = data['message']['text']
+                    message_id = data['message']['message_id']
 
-                        bot_logic(telegram_bot.id, chat_id, chat_msg, 'message', message_id)
-                    elif 'photo' in data['message'].keys():
-                        chat_id = data['message']['chat']['id']
-                        photo_id = data['message']['photo']
-                        message_id = data['message']['message_id']
+                    bot_logic(telegram_bot.id, chat_id, chat_msg, 'message', message_id)
+                elif 'photo' in data['message'].keys():
+                    chat_id = data['message']['chat']['id']
+                    photo_id = data['message']['photo']
+                    message_id = data['message']['message_id']
 
-                        # Для тестов
-                        bot = telepot.Bot(telegram_bot.token)
+                    # Для тестов
+                    bot = telepot.Bot(telegram_bot.token)
 
-                        application = SellApplication.objects.get(id=7)
+                    application = SellApplication.objects.get(id=7)
 
-                        photo = bot.getFile(photo_id)
+                    photo = bot.getFile(photo_id)
 
-                        PhotoApplications.objects.create(
-                            application=application,
-                            photo=photo,
-                        )
+                    PhotoApplications.objects.create(
+                        application=application,
+                        photo=photo,
+                    )
 
-                        # bot_logic(telegram_bot.id, chat_id, photo_id, 'photo', message_id)
-            except Exception:
-                pass
+                    # bot_logic(telegram_bot.id, chat_id, photo_id, 'photo', message_id)
 
             return HttpResponse('ok', content_type="text/plain", status=200)
         else:
