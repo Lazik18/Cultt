6be194a903d03f99f2f87449dc81b3f4dcf5cda0
ilanikiforov <Stalker74369@yandex.ru>
2022-07-29 03:18:59 +0300

    AmoCrm

--- cultt_bot/amo_crm.py
+++ cultt_bot/amo_crm.py
@@ -58,54 +58,56 @@ class AmoCrmSession:
 
         application = SellApplication.objects.get(id=application_id)
 
-        data = {
-            "name": "Заявка из TelegramBot",
-            "custom_fields_values": [
-                {
-                    "field_id": 904315,
-                    "values": [{"value": application.cooperation_option}, ]
-                },
-                {
-                    "field_id": 904321,
-                    "values": [{"value": application.category.name}, ]
-                },
-                {
-                    "field_id": 904323,
-                    "values": [{"value": application.brand.name}, ]
-                },
-                {
-                    "field_id": 904325,
-                    "values": [{"value": application.model}, ]
-                },
-                {
-                    "field_id": 904327,
-                    "values": [{"value": application.state.name}, ]
-                },
-                {
-                    "field_id": 904329,
-                    "values": [{"value": application.defect.name}, ]
-                },
-                {
-                    "field_id": 904331,
-                    "values": [{"value": application.waiting_price}, ]
+        data = [
+            {
+                "name": "Заявка из TelegramBot",
+                "custom_fields_values": [
+                    {
+                        "field_id": 904315,
+                        "values": [{"value": application.cooperation_option}, ]
+                    },
+                    {
+                        "field_id": 904321,
+                        "values": [{"value": application.category.name}, ]
+                    },
+                    {
+                        "field_id": 904323,
+                        "values": [{"value": application.brand.name}, ]
+                    },
+                    {
+                        "field_id": 904325,
+                        "values": [{"value": application.model}, ]
+                    },
+                    {
+                        "field_id": 904327,
+                        "values": [{"value": application.state.name}, ]
+                    },
+                    {
+                        "field_id": 904329,
+                        "values": [{"value": application.defect.name}, ]
+                    },
+                    {
+                        "field_id": 904331,
+                        "values": [{"value": application.waiting_price}, ]
+                    }
+                ],
+                "_embedded": {
+                    "contacts": [{
+                        "first_name": application.name,
+                        "custom_fields_values": [
+                            {
+                                "field_id": 67725,
+                                "values": [{"value": application.tel}, ]
+                            },
+                            {
+                                "field_id": 67727,
+                                "values": [{"value": application.email}, ]
+                            }
+                        ]
+                    }]
                 }
-            ],
-            "_embedded": {
-                "contacts": [{
-                    "first_name": application.name,
-                    "custom_fields_values": [
-                        {
-                            "field_id": 67725,
-                            "values": [{"value": application.tel}, ]
-                        },
-                        {
-                            "field_id": 67727,
-                            "values": [{"value": application.email}, ]
-                        }
-                    ]
-                }]
             }
-        }
+        ]
 
         result = requests.post(f'https://{self.sub_domain}/api/v4/leads/complex', headers=headers, json=data)
 
