fd78deb72445d4887d8ff36d15d00b6e237cef95
ilanikiforov <Stalker74369@yandex.ru>
2022-07-29 06:24:08 +0300

    AmoCrm

--- cultt_bot/bot/cultt_bot.py
+++ cultt_bot/bot/cultt_bot.py
@@ -468,7 +468,7 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
         elif not application.is_photo:
             if type_message == 'photo':
                 # Скачиваем фото
-                path = Path(f'application_image/{chat_result}')
+                path = Path(f'application_image/{chat_result}.jpg')
 
                 bot.download_file(chat_result, path)
 
