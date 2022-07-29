34c998961b3e91e97ca9337b69b74493a14543b7
ilanikiforov <Stalker74369@yandex.ru>
2022-07-29 03:03:22 +0300

    AmoCrm

--- cultt_bot/views.py
+++ cultt_bot/views.py
@@ -80,5 +80,5 @@ def test(request):
     amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
 
     result = amo_crm_session.create_leads_complex(10)
-    return HttpResponse(result.decode("UTF-8"), content_type="text/plain", status=200)
+    return HttpResponse(str(result).decode("utf-8"), content_type="text/plain", status=200)
 
