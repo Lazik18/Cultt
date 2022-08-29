8054d2f8c03e4fb01fdfb13414b459e6a8479904
ilanikiforov <Stalker74369@yandex.ru>
2022-08-29 21:33:51 +0300

    Delete trash

--- cultt_bot/bot/cultt_bot.py
+++ cultt_bot/bot/cultt_bot.py
@@ -99,7 +99,7 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
         # Вариант сотрудничества
         def cooperation_option_message():
             message = telegram_bot.close_button
-            keyboard = build_keyboard('reply', [{'Отменить заявку': 'Отменить заявку'}], one_time=True)
+            keyboard = build_keyboard('reply', [{""}, {'Отменить заявку': 'Отменить заявку'}], one_time=True)
             user.send_telegram_message(message, keyboard)
 
             bot_text = telegram_bot.cooperation_option_message
@@ -344,7 +344,7 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
         # Почту пользователя
         elif application.email is None:
             if type_message == 'message':
-                if email_validation(chat_result):
+                if email_validation(chat_result.replase('.', '')):
                     application.email = chat_result
                     application.save()
 
@@ -377,194 +377,198 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
                     pass
 
                 tel_message()
-        # Категория аксессуара
-        elif application.category is None:
-            if type_message == 'message':
-                category_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                if 'category' in chat_result and CategoryOptions.objects.filter(
-                        id=chat_result.split(' ')[2]).count() == 1:
-                    try:
-                        bot.deleteMessage((chat_id, message_id))
-                    except telepot.exception.TelegramError:
-                        pass
-
-                    application.category = CategoryOptions.objects.get(id=chat_result.split(' ')[2])
-                    application.save()
-
-                    brand_message()
-                else:
-                    category_message()
-        # Бренд
-        elif application.brand is None:
-            if type_message == 'message':
-                brand_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                if 'brand' in chat_result:
-                    if len(chat_result.split(' ')) == 4:
-                        if BrandOptions.objects.filter(id=chat_result.split(' ')[3]).count() == 1:
-                            application.brand = BrandOptions.objects.get(id=chat_result.split(' ')[3])
-                            application.save()
-
-                            model_message()
-                        else:
-                            brand_message()
-                    else:
-                        brand_message(chat_result.split(' ')[2])
-                else:
-                    brand_message()
-        # Модель
-        elif application.model is None:
-            if type_message == 'message':
-                application.model = chat_result
-                application.save()
-
-                state_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                model_message()
-        # Состояние
-        elif application.state is None:
-            if type_message == 'message':
-                state_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                if 'state' in chat_result and StateOptions.objects.filter(
-                        id=chat_result.split(' ')[2]).count() == 1:
-                    application.state = StateOptions.objects.get(id=chat_result.split(' ')[2])
-                    application.save()
-
-                    defect_message()
-                else:
-                    try:
-                        bot.deleteMessage((chat_id, message_id))
-                    except telepot.exception.TelegramError:
-                        pass
-
-                    state_message()
-        # Наличие дефектов
-        elif application.defect is None:
-            if type_message == 'message':
-                state_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                if 'defect' in chat_result and DefectOptions.objects.filter(
-                        id=chat_result.split(' ')[2]).count() == 1:
-                    application.defect = DefectOptions.objects.get(id=chat_result.split(' ')[2])
-                    application.save()
-
-                    waiting_price_message()
-                else:
-                    defect_message()
-        # Ожидание по цене
-        elif application.waiting_price is None:
-            if type_message == 'message':
-                if chat_result.isdecimal():
-                    application.waiting_price = chat_result
-                    application.save()
-
-                    photo_message()
-                else:
-                    waiting_price_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                waiting_price_message()
-        # Отправка фотографий
-        elif not application.is_photo:
-            if type_message == 'photo':
-                # Скачиваем фото
-                path = Path(f'application_image/{chat_result}.jpg')
-
-                bot.download_file(chat_result, path)
-
-                with path.open(mode='rb') as f:
-                    PhotoApplications.objects.create(
-                        application=application,
-                        photo=File(f, name=path.name),
-                    )
-
-                    date_end = datetime.datetime.now()
-                    date_start = date_end - timedelta(seconds=2)
-
-                    if PhotoApplications.objects.filter(application=application, date__gte=date_start,
-                                                        date__lte=date_end).count() < 2:
-                        photo_message(photo=True)
-            elif type_message == 'data':
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
-
-                if 'is_photo' in chat_result:
-                    application.is_photo = True
-                    application.save()
-
-                    end_message()
-                else:
-                    photo_message()
-            else:
-                photo_message()
-        # Подтверждение заявки
+        # Если ветка консьерж
+        elif 'консьерж' in application.cooperation_option.name.lower():
+            user.send_telegram_message('test')
         else:
-            if type_message == 'message':
-                end_message()
-            else:
-                try:
-                    bot.deleteMessage((chat_id, message_id))
-                except telepot.exception.TelegramError:
-                    pass
+            # Категория аксессуара
+            if application.category is None:
+                if type_message == 'message':
+                    category_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
 
-                if 'end_message' in chat_result:
-                    if 'send' in chat_result:
-                        application.active = False
+                    if 'category' in chat_result and CategoryOptions.objects.filter(
+                            id=chat_result.split(' ')[2]).count() == 1:
+                        try:
+                            bot.deleteMessage((chat_id, message_id))
+                        except telepot.exception.TelegramError:
+                            pass
+
+                        application.category = CategoryOptions.objects.get(id=chat_result.split(' ')[2])
                         application.save()
 
-                        amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
-                        result = amo_crm_session.create_leads_complex(application.id)
-
-                        if json.loads(result).get('title') == 'Unauthorized':
-                            if amo_crm_session.get_access_token('refresh_token'):
-                                amo_crm_session.create_leads_complex(application.id)
+                        brand_message()
                     else:
-                        bot_text = telegram_bot.close_message
-                        user.send_telegram_message(bot_text, ReplyKeyboardRemove())
-
-                        application.delete()
-
-                    user.step = 'start_message'
-                    user.save()
-
-                    keyboard = build_keyboard('reply', [{f'{telegram_bot.start_button}': 'create_application'}],
-                                              one_time=True)
-
-                    user.send_telegram_message(telegram_bot.end_message, keyboard)
+                        category_message()
+            # Бренд
+            elif application.brand is None:
+                if type_message == 'message':
+                    brand_message()
                 else:
-                    cooperation_option_message()
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    if 'brand' in chat_result:
+                        if len(chat_result.split(' ')) == 4:
+                            if BrandOptions.objects.filter(id=chat_result.split(' ')[3]).count() == 1:
+                                application.brand = BrandOptions.objects.get(id=chat_result.split(' ')[3])
+                                application.save()
+
+                                model_message()
+                            else:
+                                brand_message()
+                        else:
+                            brand_message(chat_result.split(' ')[2])
+                    else:
+                        brand_message()
+            # Модель
+            elif application.model is None:
+                if type_message == 'message':
+                    application.model = chat_result
+                    application.save()
+
+                    state_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    model_message()
+            # Состояние
+            elif application.state is None:
+                if type_message == 'message':
+                    state_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    if 'state' in chat_result and StateOptions.objects.filter(
+                            id=chat_result.split(' ')[2]).count() == 1:
+                        application.state = StateOptions.objects.get(id=chat_result.split(' ')[2])
+                        application.save()
+
+                        defect_message()
+                    else:
+                        try:
+                            bot.deleteMessage((chat_id, message_id))
+                        except telepot.exception.TelegramError:
+                            pass
+
+                        state_message()
+            # Наличие дефектов
+            elif application.defect is None:
+                if type_message == 'message':
+                    state_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    if 'defect' in chat_result and DefectOptions.objects.filter(
+                            id=chat_result.split(' ')[2]).count() == 1:
+                        application.defect = DefectOptions.objects.get(id=chat_result.split(' ')[2])
+                        application.save()
+
+                        waiting_price_message()
+                    else:
+                        defect_message()
+            # Ожидание по цене
+            elif application.waiting_price is None:
+                if type_message == 'message':
+                    if chat_result.isdecimal():
+                        application.waiting_price = chat_result
+                        application.save()
+
+                        photo_message()
+                    else:
+                        waiting_price_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    waiting_price_message()
+            # Отправка фотографий
+            elif not application.is_photo:
+                if type_message == 'photo':
+                    # Скачиваем фото
+                    path = Path(f'application_image/{chat_result}.jpg')
+
+                    bot.download_file(chat_result, path)
+
+                    with path.open(mode='rb') as f:
+                        PhotoApplications.objects.create(
+                            application=application,
+                            photo=File(f, name=path.name),
+                        )
+
+                        date_end = datetime.datetime.now()
+                        date_start = date_end - timedelta(seconds=2)
+
+                        if PhotoApplications.objects.filter(application=application, date__gte=date_start,
+                                                            date__lte=date_end).count() < 2:
+                            photo_message(photo=True)
+                elif type_message == 'data':
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    if 'is_photo' in chat_result:
+                        application.is_photo = True
+                        application.save()
+
+                        end_message()
+                    else:
+                        photo_message()
+                else:
+                    photo_message()
+            # Подтверждение заявки
+            else:
+                if type_message == 'message':
+                    end_message()
+                else:
+                    try:
+                        bot.deleteMessage((chat_id, message_id))
+                    except telepot.exception.TelegramError:
+                        pass
+
+                    if 'end_message' in chat_result:
+                        if 'send' in chat_result:
+                            application.active = False
+                            application.save()
+
+                            amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
+                            result = amo_crm_session.create_leads_complex(application.id)
+
+                            if json.loads(result).get('title') == 'Unauthorized':
+                                if amo_crm_session.get_access_token('refresh_token'):
+                                    amo_crm_session.create_leads_complex(application.id)
+                        else:
+                            bot_text = telegram_bot.close_message
+                            user.send_telegram_message(bot_text, ReplyKeyboardRemove())
+
+                            application.delete()
+
+                        user.step = 'start_message'
+                        user.save()
+
+                        keyboard = build_keyboard('reply', [{f'{telegram_bot.start_button}': 'create_application'}],
+                                                  one_time=True)
+
+                        user.send_telegram_message(telegram_bot.end_message, keyboard)
+                    else:
+                        cooperation_option_message()
     except Exception:
         bug_trap()
