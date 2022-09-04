fafdc9a76d1a97f11a9867700c17dcb0e4ec7aba
Lazik <temychin2011@yandex.ru>
2022-09-04 17:16:58 +0300

    rename button

--- cultt_bot/bot/cultt_bot.py
+++ cultt_bot/bot/cultt_bot.py
@@ -272,7 +272,7 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
                 bot_text = telegram_bot.photo_message_2
 
                 keyboard = build_keyboard('inline', [
-                    {'Закончить': 'edit_application is_photo True'}
+                    {'Завершить отправку': 'edit_application is_photo True'}
                 ])
 
                 user.send_telegram_message(bot_text, keyboard)
