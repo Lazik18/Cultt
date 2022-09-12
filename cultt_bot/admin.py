12060f4e0eb57a2d8617a9b72a9ca126be2f9c1d
Lazik <temychin2011@yandex.ru>
2022-09-12 19:05:24 +0300

    register admin

--- cultt_bot/admin.py
+++ cultt_bot/admin.py
@@ -8,7 +8,12 @@ admin.site.register(CategoryOptions)
 admin.site.register(BrandOptions)
 admin.site.register(StateOptions)
 admin.site.register(DefectOptions)
-admin.site.register(SellApplication)
 admin.site.register(PhotoApplications)
 admin.site.register(AmoCRMData)
 admin.site.register(CooperationOption)
+
+
+@admin.register(SellApplication)
+class SellApplicationAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'email', 'tel', 'active')
+    search_fields = ('name', 'email', 'tel')
