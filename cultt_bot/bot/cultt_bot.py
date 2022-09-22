f70bb4f255afe3112196891caa9ab5a7f9c3acc8
Lazik <temychin2011@yandex.ru>
2022-09-22 10:53:22 +0300

    create my profile

--- cultt_bot/bot/cultt_bot.py
+++ cultt_bot/bot/cultt_bot.py
@@ -112,6 +112,7 @@ def my_profile(bot_id, chat_id, chat_result, type_message, message_id):
     user = TelegramUser.objects.get(chat_id=chat_id, bot=telegram_bot)
 
     if type_message == 'data':
+        telegram_bot.deleteMessage((chat_id, message_id))
         text = 'Чтобы изменить данные нажмите сбросить.\nПри создании новой заявки вы сможете их заполнить.\n'
         text += f'Имя: {user.name}\nФамилия: {user.surname}\nПочта: {user.email}\nТелефон: {user.tel}'
         keyboard = build_keyboard('inline', [{f'{telegram_bot.reset_data}': 'my_profile_button_reset_data'}])
