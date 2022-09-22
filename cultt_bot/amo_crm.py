be2c422e67c5cb3c6018be0a15e6a511a7803fd0
Lazik <temychin2011@yandex.ru>
2022-09-22 23:10:34 +0300

    amoCRM id append

--- cultt_bot/amo_crm.py
+++ cultt_bot/amo_crm.py
@@ -187,11 +187,11 @@ class AmoCrmSession:
 
         result = requests.post(f'https://{self.sub_domain}/api/v4/leads/unsorted/forms', headers=headers, json=data)
 
-        # user.amocrm_id = result.json()['_embedded']['unsorted'][0]['_embedded']['contacts'][0]['id']
-        # user.save()
+        user.amocrm_id = int(result.json()['_embedded']['unsorted'][0]['_embedded']['contacts'][0]['id'])
+        user.save()
 
         if user.name == 'test':
-            user.send_telegram_message(result.json()['_embedded']['unsorted'][0]['_embedded']['contacts'][0]['id'])
+            user.send_telegram_message(f'{user.pk} {user_id}')
 
         AmoCRMLog.objects.create(result=str(result.json()))
 
