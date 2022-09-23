93492db8992838a3d1782d0f28e7249b7b965f35
Lazik <temychin2011@yandex.ru>
2022-09-23 12:26:18 +0300

    add 'error application'

--- cultt_bot/bot/cultt_bot.py
+++ cultt_bot/bot/cultt_bot.py
@@ -505,6 +505,7 @@ def create_application(bot_id, chat_id, chat_result, type_message, message_id):
                 type_message = 'message'
             elif 'error_application state' in chat_result:
                 application.state = None
+                application.defect_finished = False
                 application.save()
                 type_message = 'message'
             elif 'error_application defect' in chat_result:
