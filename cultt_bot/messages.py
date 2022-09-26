69df1f22843ae8f1b1e3ce544f8ccd834ead8bd5
Lazik <temychin2011@yandex.ru>
2022-09-26 20:51:30 +0300

    new version

--- cultt_bot/messages.py
+++ cultt_bot/messages.py
@@ -806,7 +806,10 @@ def handler_call_back(data):
             user.step = f'Model {application.cooperation_option.pk}'
             user.save()
 
-            bot.sendMessage(chat_id=user_telegram_id, text=bot_settings.other_model)
+            keyboard = [[InlineKeyboardButton(text=bot_settings.back_button, callback_data='CancelApp')]]
+            keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+            bot.sendMessage(chat_id=user_telegram_id, text=bot_settings.other_model, reply_markup=keyboard)
             return
         else:
             application.model = ModelsOption.objects.get(pk=model_id).name
