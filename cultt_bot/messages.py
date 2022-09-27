6dd38606a8982f820e7599e49d4a9945f2605a68
Lazik <temychin2011@yandex.ru>
2022-09-27 17:45:34 +0300

    test

--- cultt_bot/messages.py
+++ cultt_bot/messages.py
@@ -246,7 +246,7 @@ def create_applications(user_telegram_id, coop_option_id, last_step=None, letter
         user.save()
 
         if last_step is not None:
-            line_keyboard.append([InlineKeyboardButton(text=bot_settings.back_button, callback_data=f'BackApp {last_step}')])
+            line_keyboard.append(InlineKeyboardButton(text=bot_settings.back_button, callback_data=f'BackApp {last_step}'))
         line_keyboard.append(cancel_keyboard)
 
         keyboard.append(line_keyboard)
