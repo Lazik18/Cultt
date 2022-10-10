4cdf7910da804d212bce9c54391a5598bf3da7de
Lazik <temychin2011@yandex.ru>
2022-10-10 17:03:08 +0300

    test

--- cultt_bot/messages.py
+++ cultt_bot/messages.py
@@ -532,7 +532,7 @@ def handler_message(data):
 
     application = SellApplication.objects.filter(user=user, active=True).first()
 
-    if bot_settings.close_button == message_text:
+    if bot_settings.cancel_applications in message_text:
         application.delete()
 
         keyboard = [[InlineKeyboardButton(text=bot_settings.start_button, callback_data='MainMenu')],
