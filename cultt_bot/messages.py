ac7854efc3a978327a463694f53b0b15f22085a0
Lazik <temychin2011@yandex.ru>
2022-10-17 21:01:20 +0300

    test

--- cultt_bot/messages.py
+++ cultt_bot/messages.py
@@ -420,7 +420,7 @@ def create_applications(user_telegram_id, coop_option_id, last_step=None, letter
         if application.concierge_count != 0:
             bot_text += bot_settings.applications_concierge_count + f': {application.concierge_count}\n'
 
-        keyboard = [[InlineKeyboardButton(text=bot_settings.cancel_applications, callback_data='CancelApp'),
+        keyboard = [[# InlineKeyboardButton(text=bot_settings.cancel_applications, callback_data='CancelApp'),
                      InlineKeyboardButton(text=bot_settings.error_application, callback_data=f'EditApp'),
                      InlineKeyboardButton(text=bot_settings.send_application_button, callback_data='SendApp')]]
 
