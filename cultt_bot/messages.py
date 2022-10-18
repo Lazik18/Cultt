8328cc826e56ff47de42ebd0621efdfce3c6f663
Lazik <temychin2011@yandex.ru>
2022-10-18 03:13:08 +0300

    stats update

--- cultt_bot/messages.py
+++ cultt_bot/messages.py
@@ -1020,7 +1020,7 @@ def handler_call_back(data):
 
         keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard)
 
-        text = bot_settings.end_message + str(application.id) + "\nХотите получать уведомления?"
+        text = bot_settings.end_message + f' {application.id}\nХотите получать уведомления?'
 
         bot.sendMessage(chat_id=user_telegram_id, text=text, reply_markup=keyboard)
     elif 'CreateApp' in button_press:
