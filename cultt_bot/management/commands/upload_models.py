989579fbbf87571bdd8dd82b89f7bc159e4776b9
Lazik <temychin2011@yandex.ru>
2022-11-04 21:55:06 +0300

    add upload models command

---
+++ cultt_bot/management/commands/upload_models.py
@@ -0,0 +1,13 @@
+import json
+
+import pandas as pd
+from django.core.management import BaseCommand
+
+from cultt_bot.models import SellApplication
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        data = pd.read_excel('static/new_model.xlsx', sheet_name='Лист1')
+
+        print(data)
