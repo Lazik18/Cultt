0c5b333420b097358fd24be359b11eb7793bd322
Lazik <temychin2011@yandex.ru>
2022-11-04 11:38:57 +0300

    test

--- cultt_bot/management/commands/upload_app.py
+++ cultt_bot/management/commands/upload_app.py
@@ -6,7 +6,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         data = pd.read_excel('static/app.xlsx', sheet_name='Лист1')
 
-        data1 = data[1].to_dict()
+        print(data)
+
+        data1 = data[2].to_dict()
 
         for i in data1:
             print(i)
