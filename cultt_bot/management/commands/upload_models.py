994d18417bcea5f64eec94098e004793a84d4547
Lazik <temychin2011@yandex.ru>
2022-11-04 22:25:34 +0300

    test

--- cultt_bot/management/commands/upload_models.py
+++ cultt_bot/management/commands/upload_models.py
@@ -23,4 +23,7 @@ class Command(BaseCommand):
                 models = data[data['Тип аксессуара'] == category][data['Бренд'] == brand]['Модель'].tolist()
 
                 for model in models:
-                    print(f'--{model}')
+                    if model == 'nan':
+                        print('--passssssssssss')
+                    else:
+                        print(f'--{model}')
