291494233d7d1db1eba9c618adacb44541b90ce7
Lazik <temychin2011@yandex.ru>
2022-11-04 11:57:47 +0300

    test

--- cultt_bot/management/commands/upload_app.py
+++ cultt_bot/management/commands/upload_app.py
@@ -1,5 +1,10 @@
+import json
+
 import pandas as pd
 from django.core.management import BaseCommand
+
+from cultt_bot.amo_crm import AmoCrmSession
+from cultt_bot.models import SellApplication
 
 
 class Command(BaseCommand):
@@ -11,4 +16,15 @@ class Command(BaseCommand):
         data1 = data[1].to_dict()
 
         for value in list(data1.values()):
-            print(value)
+            app = SellApplication.objects.get(pk=value)
+
+            amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
+            result = amo_crm_session.create_leads_complex(value, app.user)
+
+            if json.loads(result).get('title') == 'Unauthorized':
+                if amo_crm_session.get_access_token('refresh_token'):
+                    amo_crm_session.create_leads_complex(value, app.user)
+
+            print(f'{value} OK\n')
+
+        print('OK')
