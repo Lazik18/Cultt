319c967c07022a44554703b4264fcd3a7701e1ac
Lazik <temychin2011@yandex.ru>
2022-11-04 22:43:17 +0300

    test

--- cultt_bot/management/commands/upload_models.py
+++ cultt_bot/management/commands/upload_models.py
@@ -15,15 +15,32 @@ class Command(BaseCommand):
         categories = list(set(data['Тип аксессуара'].tolist()))
 
         for category in categories:
-            print(f'{category}')
+            print(f'Категория {category}')
             brands = list(set(data[data['Тип аксессуара'] == category]['Бренд'].tolist()))
+
+            category_option = CategoryOptions.objects.filter(name=category).first()
+
+            if category_option is None:
+                print(f'Нет категории - {category}')
+                continue
 
             for brand in brands:
                 print(f'-{brand}')
                 models = data[data['Тип аксессуара'] == category][data['Бренд'] == brand]['Модель'].tolist()
 
+                brand_option = BrandOptions.objects.filter(category=category_option, name__in=brand).first()
+
+                if brand_option is None:
+                    brand_option = BrandOptions.objects.create(category=category_option, name=brand)
+                    print(f'Создан бренд - {brand}')
+
                 for model in models:
                     if model is not str:
-                        print('passsssss')
-                    else:
-                        print(f'--{model} {type(model)}')
+                        continue
+
+                    if ModelsOption.objects.exists(brand=brand_option, name__in=model):
+                        print(f'Модель уже существует - {model}')
+                        continue
+
+                    ModelsOption.objects.create(brand=brand_option, name=model)
+                    print(f'Модель создана - {model}')
