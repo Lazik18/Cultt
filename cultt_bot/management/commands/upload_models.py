54f23311c0142f0f3bc23f5ca7c758e2436f0952
Lazik <temychin2011@yandex.ru>
2022-11-04 22:29:14 +0300

    test

--- cultt_bot/management/commands/upload_models.py
+++ cultt_bot/management/commands/upload_models.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
                 models = data[data['Тип аксессуара'] == category][data['Бренд'] == brand]['Модель'].tolist()
 
                 for model in models:
-                    if model <= 0:
+                    if model is not str:
                         print('passsssss')
                     else:
                         print(f'--{model} {type(model)}')
