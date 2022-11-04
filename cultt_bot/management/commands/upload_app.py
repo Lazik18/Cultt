7f6cb963daf24b4509882045c9b58cae0911944a
Lazik <temychin2011@yandex.ru>
2022-11-04 11:35:27 +0300

    test

---
+++ cultt_bot/management/commands/upload_app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        data = pd.read_excel('app.xlsx', sheet_name='Лист1')
+
+        data1 = data[1].to_dict()
+
+        for i in data1:
+            print(i)
