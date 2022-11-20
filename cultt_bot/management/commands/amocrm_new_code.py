ca2dcf71896cabcfa45a7283528b7e69e3a8a425
Lazik <temychin2011@yandex.ru>
2022-11-20 21:36:22 +0300

    add amocrm command

--- cultt_bot/management/commands/amocrm_new_code.py
+++ cultt_bot/management/commands/amocrm_new_code.py
@@ -1,5 +1,10 @@
+from django.core.management import BaseCommand
+
 from cultt_bot.amo_crm import AmoCrmSession
 
-amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
-result = amo_crm_session.get_access_token()
-print(result)
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
+        result = amo_crm_session.get_access_token()
+        print(result)
