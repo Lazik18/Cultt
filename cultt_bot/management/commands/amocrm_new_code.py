98e585ca414dedd6d41f250cb79004b5909e71c5
Lazik <temychin2011@yandex.ru>
2022-11-20 21:31:05 +0300

    add amocrm command

--- cultt_bot/management/commands/amocrm_new_code.py
+++ cultt_bot/management/commands/amocrm_new_code.py
@@ -1,4 +1,5 @@
 from cultt_bot.amo_crm import AmoCrmSession
 
 amo_crm_session = AmoCrmSession('thecultt.amocrm.ru')
-amo_crm_session.get_access_token()
+result = amo_crm_session.get_access_token()
+print(result)
