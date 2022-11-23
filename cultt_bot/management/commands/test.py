f7c92747194033c1c47cb812b44acda643f672e7
Lazik <temychin2011@yandex.ru>
2022-11-23 10:37:54 +0300

    test

--- cultt_bot/management/commands/test.py
+++ cultt_bot/management/commands/test.py
@@ -1,8 +1,16 @@
 import requests
 from django.core.management import BaseCommand
 
+from cultt_bot.amo_crm import AmoCrmSession
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        resp = requests.get(f'https://thecultt.amocrm.ru/api/v4/contacts?filter[custom_fields_values][67727]=buzzina@rambler.ru')
+        amo_crm_data = AmoCrmSession('thecultt.amocrm.ru')
+        headers = {
+            'authorization': f'Bearer {amo_crm_data.get_access_token()}',
+            'Content-Type': 'application/json'
+        }
+
+        resp = requests.get(f'https://thecultt.amocrm.ru/api/v4/contacts?filter[custom_fields_values][67727]=buzzina@rambler.ru', headers)
         print(resp.json())
