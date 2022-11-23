ecb787833bc357dc73de0d619e2cb90ca31f51a3
Lazik <temychin2011@yandex.ru>
2022-11-23 10:30:42 +0300

    test

--- cultt_bot/management/commands/test.py
+++ cultt_bot/management/commands/test.py
@@ -5,4 +5,4 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         resp = requests.get(f'https://thecultt.amocrm.ru/api/v4/contacts?filter[custom_fields_values][67727]=buzzina@rambler.ru')
-        print(resp)
+        print(resp.json())
