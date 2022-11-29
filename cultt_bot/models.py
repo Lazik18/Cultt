997d96a314f07bc1cf51d867a605671471be7e58
Lazik <temychin2011@yandex.ru>
2022-11-29 12:56:28 +0300

    add field amocrm for Cooperation Option

--- cultt_bot/models.py
+++ cultt_bot/models.py
@@ -266,6 +266,10 @@ class CooperationOption(models.Model):
     price = models.BooleanField(default=False, verbose_name='Цена')
     photo = models.BooleanField(default=False, verbose_name='Фото')
 
+    amocrm_pipeline_id = models.IntegerField(default=0, verbose_name='ID воронки в амосрм')
+    amocrm_status_id = models.IntegerField(default=0, verbose_name='ID статуса в амосрм')
+    amocrm_tag = models.TextField(default='бот', verbose_name='Тег в амосрм')
+
 
     def __str__(self):
         return self.name
