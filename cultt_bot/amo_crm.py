e7fa204425ecff0143a04db3e32fe4bd4bcc1588
Lazik <temychin2011@yandex.ru>
2022-11-30 12:04:36 +0300

    add tag

--- cultt_bot/amo_crm.py
+++ cultt_bot/amo_crm.py
@@ -223,7 +223,7 @@ class AmoCrmSession:
             except Exception as ex:
                 TelegramLog.objects.create(text=repr(ex) + '\n' + traceback.format_exc())
 
-        data[0]['_embedded']['contacts'][0]['_embedded'] = {'tags': [{'name': "Новая регистрация"}]}
+        #data[0]['_embedded']['contacts'][0]['_embedded'] = {'tags': [{'name': "Новая регистрация"}]}
 
         if user.amocrm_id is not None:
             data[0]['_embedded']['contacts'][0]['id'] = user.amocrm_id
@@ -255,8 +255,14 @@ class AmoCrmSession:
         except Exception as ex:
             TelegramLog.objects.create(text=repr(ex) + '\n' + traceback.format_exc())
 
-        status_data = {'status_id': application.cooperation_option.amocrm_status_id}
+        tags_contact_data = {'_embedded': {'tags': [{'name': "Новая регистрация"}]}}
+        requests.patch(f'https://{self.sub_domain}/api/v4/contacts/{user.amocrm_id}',
+                       headers=headers,
+                       params=tags_contact_data)
 
-        result_status = requests.patch(f'https://{self.sub_domain}/api/v4/leads/{application.amocrm_id}', headers=headers, params=status_data)
+        status_data = {'status_id': application.cooperation_option.amocrm_status_id}
+        requests.patch(f'https://{self.sub_domain}/api/v4/leads/{application.amocrm_id}',
+                       headers=headers,
+                       params=status_data)
 
         return result.text
