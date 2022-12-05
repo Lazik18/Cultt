7cdc7e3d1ec5748e560d8999d62deb40e230816d
Lazik <temychin2011@yandex.ru>
2022-12-06 02:02:26 +0300

    test

--- cultt_bot/management/commands/upload_app.py
+++ cultt_bot/management/commands/upload_app.py
@@ -1,46 +1,12 @@
-import json
-
-import pandas as pd
 from django.core.management import BaseCommand
 
-from cultt_bot.models import CategoryOptions, BrandOptions, ModelsOption
+from cultt_bot.models import SellApplication
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        data = pd.read_excel('static/new_model.xlsx', sheet_name='list 1')
+        applications = SellApplication.objects.all()
 
-        print(data)
-
-        categories = list(set(data['Тип аксессуара'].tolist()))
-
-        for category in categories:
-            print(f'=== Категория {category} ===')
-            brands = list(set(data[data['Тип аксессуара'] == category]['Бренд'].tolist()))
-
-            category_option = CategoryOptions.objects.filter(name=category).first()
-
-            if category_option is None:
-                print(f'Нет категории - {category}')
-                continue
-
-            for brand in brands:
-                print(f'-{brand}')
-                models = data[data['Тип аксессуара'] == category][data['Бренд'] == brand]['Модель'].tolist()
-
-                brand_option = BrandOptions.objects.filter(category=category_option, name__istartswith=brand).first()
-
-                if brand_option is None:
-                    brand_option = BrandOptions.objects.create(category=category_option, name=brand)
-                    print(f'Создан бренд - {brand}')
-
-                for model in models:
-                    if type(model) is not str:
-                        continue
-
-                    if ModelsOption.objects.filter(brand=brand_option, name__istartswith=model).exists():
-                        print(f'Модель уже существует - {model}')
-                        continue
-
-                    ModelsOption.objects.create(brand=brand_option, name=model)
-                    print(f'Модель создана - {model}')
+        for application in applications:
+            application.status = 'Ваша заявка находится в обработке. Менеджер свяжется с вами в ближайшее время для обсуждения деталей.'
+            application.save()
