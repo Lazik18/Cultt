d1d43caf7c60667960df01b65459a0a79ba6ed40
Lazik <temychin2011@yandex.ru>
2022-12-26 17:48:45 +0300

    test download

--- cultt_bot/views.py
+++ cultt_bot/views.py
@@ -170,7 +170,7 @@ def web_hook_amocrm(request):
 
 
 def download_file(request):
-    filename = 'new_models.csv'
+    filename = 'new_model.csv'
     filepath = BASE_DIR + '/static/' + filename
     path = open(filepath, 'r')
     mime_type, _ = mimetypes.guess_type(filepath)
