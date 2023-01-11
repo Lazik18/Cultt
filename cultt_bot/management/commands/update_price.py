be61bb6ec669bddfcc041cec190fc7455d39c918
Lazik <temychin2011@yandex.ru>
2023-01-11 16:54:06 +0300

    fix

--- cultt_bot/management/commands/update_price.py
+++ cultt_bot/management/commands/update_price.py
@@ -22,12 +22,12 @@ class Command(BaseCommand):
             print(f'Бренд: {brand}')
             models = data[data['Бренд'] == brand]['Модель'].tolist()
 
-            brand_option = BrandOptions.objects.filter(category=category_option, name__icontains=brand).first()
+            brand_option = BrandOptions.objects.filter(category=category_option, name__istartswith=brand).first()
 
             for model in models:
-                if ModelsOption.objects.filter(brand=brand_option, name__icontains=model[:-1]).exists():
+                if ModelsOption.objects.filter(brand=brand_option, name__istartswith=model[:-1]).exists():
                     print(f'Модель {model}')
-                    model_option = ModelsOption.objects.filter(brand=brand_option, name__icontains=model[:-1]).first()
+                    model_option = ModelsOption.objects.filter(brand=brand_option, name__istartswith=model[:-1]).first()
                     model_option.have_offer_price = True
 
                     if data[data['Бренд'] == brand][data['Модель'] == model]['Приоритет выкуп'].tolist()[0] is not None:
