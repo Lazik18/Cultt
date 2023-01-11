563f1f63abd44c1b55babf77d5eebf1c6c024f93
Lazik <temychin2011@yandex.ru>
2023-01-11 15:48:43 +0300

    add update price command

--- cultt_bot/management/commands/update_price.py
+++ cultt_bot/management/commands/update_price.py
@@ -30,53 +30,53 @@ class Command(BaseCommand):
                     model_option = ModelsOption.objects.filter(brand=brand_option, name__istartswith=model).first()
                     model_option.have_offer_price = True
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Приоритет выкуп'] is not None:
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Приоритет выкуп'].tolist()[0] is not None:
                         model_option.offer_priority = True
 
-                    model_option.price_site_min = data[data['Бренд'] == brand][data['Модель'] == model]['Цена сайта min (базовая)']
-                    model_option.price_site_max = data[data['Бренд'] == brand][data['Модель'] == model]['Цена сайта max (базовая)']
+                    model_option.price_site_min = data[data['Бренд'] == brand][data['Модель'] == model]['Цена сайта min (базовая)'].tolist()[0]
+                    model_option.price_site_max = data[data['Бренд'] == brand][data['Модель'] == model]['Цена сайта max (базовая)'].tolist()[0]
 
-                    model_option.price_purchase_min = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по выкупу min']
-                    model_option.price_purchase_max = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по выкупу max']
+                    model_option.price_purchase_min = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по выкупу min'].tolist()[0]
+                    model_option.price_purchase_max = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по выкупу max'].tolist()[0]
 
-                    model_option.price_sale_min = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по реализации min']
-                    model_option.price_sale_max = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по реализации max']
+                    model_option.price_sale_min = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по реализации min'].tolist()[0]
+                    model_option.price_sale_max = data[data['Бренд'] == brand][data['Модель'] == model]['Выплата по реализации max'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер S'] != '-':
-                        model_option.size_S = data[data['Бренд'] == brand][data['Модель'] == model]['Размер S']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер S'].tolist()[0] != '-':
+                        model_option.size_S = data[data['Бренд'] == brand][data['Модель'] == model]['Размер S'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер M'] != '-':
-                        model_option.size_M = data[data['Бренд'] == brand][data['Модель'] == model]['Размер M']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер M'].tolist()[0] != '-':
+                        model_option.size_M = data[data['Бренд'] == brand][data['Модель'] == model]['Размер M'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер L'] != '-':
-                        model_option.size_L = data[data['Бренд'] == brand][data['Модель'] == model]['Размер L']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Размер L'].tolist()[0] != '-':
+                        model_option.size_L = data[data['Бренд'] == brand][data['Модель'] == model]['Размер L'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Яркие/Лимитированные'] != '-':
-                        model_option.color_L = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Яркие/Лимитированные']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Яркие/Лимитированные'].tolist()[0] != '-':
+                        model_option.color_L = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Яркие/Лимитированные'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Нейтральные'] != '-':
-                        model_option.color_N = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Нейтральные']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Нейтральные'].tolist()[0] != '-':
+                        model_option.color_N = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Нейтральные'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Классические'] != '-':
-                        model_option.color_C = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Классические']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Классические'].tolist()[0] != '-':
+                        model_option.color_C = data[data['Бренд'] == brand][data['Модель'] == model]['Цвет Классические'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Винтаж'] != '-':
-                        model_option.state_V = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Винтаж']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Винтаж'].tolist()[0] != '-':
+                        model_option.state_V = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Винтаж'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Хорошее'] != '-':
-                        model_option.state_G = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Хорошее']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Хорошее'].tolist()[0] != '-':
+                        model_option.state_G = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Хорошее'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Отличное'] != '-':
-                        model_option.state_E = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Отличное']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Отличное'].tolist()[0] != '-':
+                        model_option.state_E = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Отличное'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Новое с биркой'] != '-':
-                        model_option.state_N = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Новое с биркой']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Новое с биркой'].tolist()[0] != '-':
+                        model_option.state_N = data[data['Бренд'] == brand][data['Модель'] == model]['Состояние Новое с биркой'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Текстиль/Рафия/PVX'] != '-':
-                        model_option.material_T = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Текстиль/Рафия/PVX']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Текстиль/Рафия/PVX'].tolist()[0] != '-':
+                        model_option.material_T = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Текстиль/Рафия/PVX'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Кожа/Замша'] != '-':
-                        model_option.material_L = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Кожа/Замша']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Кожа/Замша'].tolist()[0] != '-':
+                        model_option.material_L = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Кожа/Замша'].tolist()[0]
 
-                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Экзотическая кожа'] != '-':
-                        model_option.material_EL = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Экзотическая кожа']
+                    if data[data['Бренд'] == brand][data['Модель'] == model]['Материал Экзотическая кожа'].tolist()[0] != '-':
+                        model_option.material_EL = data[data['Бренд'] == brand][data['Модель'] == model]['Материал Экзотическая кожа'].tolist()[0]
